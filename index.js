d31780009e783f39e442408cfbadd8ba46935974
pemrouz <p3pedram@hotmail.com>
2015-07-09 03:22:05 +0100

    add configurable time option

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-module.exports = function debounce(fn){
-  var wait = 100, pending
+var is = require('is')
 
-  return function(){
-    var ctx = this, args = arguments
-    pending && clearTimeout(pending)
-    pending = setTimeout(function(){ fn.apply(ctx, args) }, wait)
+module.exports = function debounce(d){
+  var pending, wait = is.num(d) ? d : 100
+
+  return is.fn(d) 
+       ? next(d)
+       : next
+
+  function next(fn){
+    return function(){
+      var ctx = this, args = arguments
+      pending && clearTimeout(pending)
+      pending = setTimeout(function(){ fn.apply(ctx, args) }, wait)
+    }
   }
+  
 }
--- test.js
+++ test.js
@@ -22,4 +22,23 @@ describe('debounce', function() {
     setTimeout(done, 600)
   })
 
+  it('should debounce fn after custom time', function(done) {
+    var fn = function(){ result++ }
+      , result = 0
+      , debounced = debounce(200)(fn)
+
+    debounced()    
+    debounced()    
+    debounced()    
+
+    setTimeout(debounced, 100)
+    setTimeout(debounced, 200)
+    setTimeout(debounced, 300)
+    setTimeout(function(){ expect(result).to.be.eql(0) }, 400)
+    setTimeout(function(){ expect(result).to.be.eql(1) }, 800)
+    setTimeout(debounced, 820)
+    setTimeout(function(){ expect(result).to.be.eql(2) }, 1180)
+    setTimeout(done, 1200)
+  })
+
 })
